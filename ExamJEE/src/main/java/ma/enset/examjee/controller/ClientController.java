71107c080d2bc71f56861dc00208194aa9bf2f7d
boggyman0 <omarkhabou46@gmail.com>
2026-05-11 10:53:03 +0100

    add client controller

---
+++ ExamJEE/src/main/java/ma/enset/examjee/controller/ClientController.java
@@ -0,0 +1,40 @@
+package ma.enset.examjee.controller;
+
+import lombok.RequiredArgsConstructor;
+import ma.enset.examjee.dto.ClientDTO;
+import ma.enset.examjee.dto.ContratAssuranceDTO;
+import ma.enset.examjee.service.IClientService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/clients")
+@RequiredArgsConstructor
+public class ClientController {
+    private final IClientService clientService;
+
+
+    @PostMapping
+    public ResponseEntity<ClientDTO> createClientDto(@RequestBody ClientDTO clientDTO) {
+        return new ResponseEntity<>(clientService.createClient(clientDTO), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ClientDTO>> getAllClients() {
+        return new ResponseEntity<>(clientService.getAllClients(), HttpStatus.OK);
+    }
+
+    @GetMapping("{nom}")
+    public ResponseEntity<ClientDTO> getClientByNom(@PathVariable String nom) {
+        return new ResponseEntity<>(clientService.getClientByNom(nom), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}/contrats")
+    public ResponseEntity<List<ContratAssuranceDTO>> getContratAssuranceByCid(@PathVariable long id) {
+        return new ResponseEntity<>(clientService.getListContratbyClientId(id), HttpStatus.OK);
+    }
+
+}
